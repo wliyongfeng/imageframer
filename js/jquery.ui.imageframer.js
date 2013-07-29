7f89b48dd6029721c434e93cd81e8ed5fcd43209
Yongfeng LI <li_yo@worksap.co.jp>
2013-07-29 10:17:01 +0000

    init my first jQuery ui widget

---
+++ js/jquery.ui.imageframer.js
@@ -0,0 +1,32 @@
+(function($) {
+
+    $.widget("ui.imageframer", {
+		options: {
+			borderColor: "pink",
+			color: "#fff",
+			backgroundColor: "pink",
+			width: "200px"
+		},
+				
+		_create: function() {
+			var self = this,
+			divElement = self.element,
+			options = self.options;
+			$(divElement).css({
+				"border-color": options.borderColor,
+				"border-style": "solid",
+				"border-width": "2px",
+				"display":"block",
+				"width": options.width});
+
+			var $img = $(divElement).children("img");
+			$img.css({'width':options.width});
+			cap = $("<span></span>").text($img.attr("alt")).css({
+				"backgroundColor":options.backgroundColor,
+				"color":options.color,
+				display:"block"
+			}).insertAfter($img);
+		},
+				
+	});
+})(jQuery);
